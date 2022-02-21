0d26cb481a4dbd26d8b9f3bbb7810008f11027ee
SilentDubich <kirill.dubov.2012@mail.ru>
2022-02-21 22:07:55 +0300

    +++

---
+++ src/Main.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import './App.css';
+export const Main: FC<{}> = () => {
+	return (
+		<div>
+			<div>Морской бой</div>
+			<div>
+				<div>Одиночная игра</div>
+				<div>Сетевая игра</div>
+			</div>
+		</div>
+	);
+}
